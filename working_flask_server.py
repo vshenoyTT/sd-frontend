13266e544f4ac795cc32faf10f01110939172c47
vshenoyTT <vshenoy@tenstorrent.com>
2024-07-23 09:39:01 -0700

    morn

--- working_flask_server.py
+++ working_flask_server.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    message = "Hello world!"
+    return message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
